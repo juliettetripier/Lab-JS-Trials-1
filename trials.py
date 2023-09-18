845761bbfed244cc60a4caca41d702b0dbf8b9fb
Juliette <juli.tripier@gmail.com>
2023-09-18 16:00:24 -0400

    Finished lab

--- trials.py
+++ trials.py
@@ -2,40 +2,92 @@
 
 
 def output_all_items(items):
-    pass  # TODO: replace this line with your code
+    for item in items:
+        print(item)
 
 
 def get_all_evens(nums):
-    pass  # TODO: replace this line with your code
+    even_nums = []
+
+    for num in nums:
+        if num % 2 == 0:
+            even_nums.append(num)
+    
+    return even_nums
 
 
 def get_odd_indices(items):
-    pass  # TODO: replace this line with your code
+    result = []
+
+    for i, item in enumerate(items):
+        if i % 2 != 0:
+            result.append(item[i])
+    
+    return result
+
 
 
 def print_as_numbered_list(items):
-    pass  # TODO: replace this line with your code
+    i = 1
+
+    for item in items:
+        print(f'{i}. {item}')
+        i += 1
 
 
 def get_range(start, stop):
-    pass  # TODO: replace this line with your code
+    nums = []
+
+    for i in range(start, stop):
+        nums.append(i)
+
+    return nums
+
 
 
 def censor_vowels(word):
-    pass  # TODO: replace this line with your code
+    chars = []
+
+    for letter in word:
+        if letter == 'a' or letter == 'e' or letter == 'i' or letter == 'o' or letter == 'u':
+            chars.append('*')
+        else:
+            chars.append(letter)
+    
+    return ''.join(chars)
+
 
 
 def snake_to_camel(string):
-    pass  # TODO: replace this line with your code
+    camel_case = []
+
+    for word in string.split('_'):
+        camel_case.append(word[0].upper())
+        camel_case.append(word[1:])
+    
+    return ''.join(camel_case)
+
 
 
 def longest_word_length(words):
-    pass  # TODO: replace this line with your code
+    longest = len(words[0])
+
+    for word in words:
+        if longest < len(word):
+            longest = len(word)
+    
+    return longest
 
 
 def truncate(string):
-    pass  # TODO: replace this line with your code
+    result = []
 
+    for char in string:
+        if len(result) == 0 or char != result[len(result)-1]:
+            result.append(char)
+    
+    return ''.join(result)
+print(truncate('aaaabbbbcccca'))
 
 def has_balanced_parens(string):
     pass  # TODO: replace this line with your code
